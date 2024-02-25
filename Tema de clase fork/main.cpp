33cbdabb749db1a911ca91fc9988139478e6247b
lrivasc4 <lrivasc4@miumg.edu.gt>
2024-02-25 11:57:05 -0600

    Initial Commit

---
+++ Tema de clase fork/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Tema de clase fork
+//
+//  Created by Luis Rivas on 25/02/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
